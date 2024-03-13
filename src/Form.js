78ae0c139e99f8e1184742acf09e46d894474c47
mwangi-george <mwangigeorge648@gmail.com>
2024-03-13 17:44:48 +0300

    Storing forms data

--- src/Form.js
+++ src/Form.js
@@ -1,14 +1,36 @@
+import { useState } from "react";
+
 function Form(){
+  // react state to store data 
+  const [form, setForm] = useState({name: "", email: ""})
+
   const handleSubmit = (event) => {
     // prevents logs from disappearing after one second
     event.preventDefault();
 
     console.log("Submitted Successfully!");
+    console.log(form);
+  }
+
+  // function to execute each time a character is written in the name input field
+  const onChange = (event) => {
+    console.log("Name field Changed");
+
+    // two variables storing data
+    const name = event.target.name;
+    const value = event.target.value;
+
+    console.log("field is " + name + " and value is " + value);
+
+    // maintain the structure of the state
+    setForm({...form, "name": value})
+
   }
   
   // a html form with a submit button
   return(
     <form onSubmit={handleSubmit}>
+      <input type="text" name="name" onChange={onChange} placeholder="First Name"></input>
       <button type="submit"> Submit Form</button>
     </form>
   );
