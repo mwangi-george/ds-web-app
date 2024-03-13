273289b7ef83db2e60b0716c4de828d6bb0604dd
mwangi-george <mwangigeorge648@gmail.com>
2024-03-13 13:23:17 +0300

    Created my first component - User

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import logo from './logo.svg';
 import './App.css';
+import User from './User';
 
 function App() {
   return (
@@ -17,6 +18,7 @@ function App() {
         >
           Learn React
         </a>
+        <User />7
       </header>
     </div>
   );
---
+++ src/User.js
@@ -0,0 +1,7 @@
+function User(){
+  return (
+    <h1> User data here!</h1>
+  );
+}
+
+export default User;
