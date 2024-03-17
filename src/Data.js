7508df10440c442329fd189c0901baa04600fe7d
mwangi-george <mwangigeorge648@gmail.com>
2024-03-17 17:19:44 +0300

    API

--- src/Data.js
+++ src/Data.js
@@ -1,17 +1,33 @@
+import { useState } from "react";
+
 function Data(){
+  const [user, setUser] = useState(null)
   const getData = () => {
     fetch("https://randomuser.me/api/")
       .then(response => response.json())
       .then(data => {
         console.log(data);
+        setUser(data.results[0]);
       })
   }
   return(
     <div>
       <button onClick={getData}>Fetch Data</button>
-      <p>George -- george@example.com</p>
+      {user ? <p>{user.name.first} {user.name.last}- {user.email}</p> : <p>User Not Found</p>}
     </div>
   );
 }
+
+/*
+Module Resources
+The following links will help you get more into detail of react. Attached you will also find all the source codes of each lecture of this section.
+
+React Hooks: https://reactjs.org/docs/hooks-overview.html (It would be great for you to read about them and play with them. There are great hooks that will be usefull in the future like useEffect, useRef or create your own hooks).
+
+React useState: https://reactjs.org/docs/hooks-reference.html#usestate
+
+JavaScript Event: https://developer.mozilla.org/en-US/docs/Web/API/Event (we talked about this in the Build our First Form lecture. This will explain with more detail how does it work and what properties can you use.)
+
+Fetch: https://developer.mozilla.org/en-US/docs/Web/API/Fetch_API/Using_Fetch */
 
 export default Data;
